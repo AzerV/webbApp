d1c14608cb0c13a34872f995b645f383b1b301bd
elinkarlsson <elikarl@ed5355-11.ed5355.studat.a.1.hb.cdal.chalmers.se>
2013-09-16 15:36:40 +0200

    Steg 1 Test

---
+++ src/main/java/org/elikarl/catchatmodel/User.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.elikarl.catchatmodel;
+
+/**
+ *
+ * @author elikarl
+ */
+public class User {
+    String name;
+    String password;
+    
+    public User(String name, String password){
+        this.name = name;
+        this.password = password;
+    }
+    
+    private String getPasszord(){
+        return password;
+    }
+    private String getName(){
+        return name;
+    }
+    
+}
